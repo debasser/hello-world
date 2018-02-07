de78acc065a15727de673abf35549e3e46600989
LAUTRIV\cboyd <cboyd@accurence.com>
2018-02-07 15:49:24 -0700

    and another commit

--- HelloGitWorld/HelloGitWorld/Program.cs
+++ HelloGitWorld/HelloGitWorld/Program.cs
@@ -13,6 +13,7 @@ namespace HelloGitWorld
             Console.WriteLine("Messing around with git");
             Console.WriteLine("And having a ball to boot");
             Console.WriteLine("Here is a commit");
+            Console.WriteLine("And another commit");
         }
     }
 }
